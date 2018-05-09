006a29aa19959f2419eed6671a56235804d55045
Justin Rigler <justinmrigler@gmail.com>
2018-05-09 00:23:02 -0400

    created post schema

--- src/post.js
+++ src/post.js
@@ -9,6 +9,43 @@ mongoose.connect('mongodb://localhost/so-posts');
 
 const PostSchema = new mongoose.Schema({
   // TODO: write your schema here
+  soID: {
+    type: Number,
+    required: true
+  },
+  parentID: {
+    type: Number
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true
+  },
+  tags: [String],
+  acceptedAnswerId: {
+    type: Number
+  },
+  user: {
+    soUserID: {
+      type: Number
+    },
+    name: {
+      type: String
+    },
+    reputation: {
+      type: Number
+    }
+  }
 });
 
 module.exports = mongoose.model('Posts', PostSchema);
